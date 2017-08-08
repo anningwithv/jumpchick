3e69ea52bba754ac9c879bce742c6b4af232f0be
zhangwei <378294113@qq.com>
2017-08-08 22:01:26 +0800

    碰撞检测完成

--- Assets/Scripts/Chick/ChickController.cs
+++ Assets/Scripts/Chick/ChickController.cs
@@ -20,18 +20,26 @@ public class ChickController : MonoBehaviour {
         Down
     }
 
-    //public Transform m_topLeft = null;
-    //public Transform m_topRight = null;
-    //public Transform m_bottomLeft = null;
-    //public Transform m_bottomRight = null;
+    public enum State
+    {
+        Idle,
+        Jump,
+        Fall,
+        Dead
+    }
 
     private InputHandler m_inputHandler = null;
     private Rigidbody2D m_rgd = null;
 
     private bool m_isJumping = false;
 
-    private float m_jumpForceY = 20f;
-    private float m_jumpForceX = 10f;
+    //private float m_jumpForceY = 20f;
+    //private float m_jumpForceX = 10f;
+    private float m_speedY = 0f;
+    private float m_maxSpeedY = 5f;
+    private float m_deltaSpeedY = 10f;
+    private float m_speedX = 0f;
+    private float m_maxSpeedX = 3f;
 
     private JumpDir m_jumpDir = JumpDir.Right;
 
@@ -43,16 +51,28 @@ public class ChickController : MonoBehaviour {
 
     private string m_checkColliderLayerName = "Brick";
 
+    private State m_state;
+    public State state {
+        get { return m_state; }
+        set { m_state = value; }
+    }
+
     void Start()
     {
         m_inputHandler = GetComponent<InputHandler>();
         m_rgd = GetComponent<Rigidbody2D>();
         m_collider2D = GetComponent<BoxCollider2D>();
 
+        Init();
+    }
+
+    private void Init()
+    {
+        state = State.Idle;
+
         SetBoundary();
         RegisterInputListener();
     }
-
     private void SetBoundary()
     {
         Vector2 center = new Vector2(transform.position.x, transform.position.y) + m_collider2D.offset;
@@ -75,29 +95,44 @@ public class ChickController : MonoBehaviour {
 
     void Update()
     {
+    }
 
+    private void UpdatePosition()
+    {
+        //transform.position += new Vector3(m_speedX*Time.deltaTime, m_speedY*Time.deltaTime, 0);
+        m_rgd.velocity = new Vector2(m_speedX, m_speedY);
+    }
+
+    private void UpdateSpeedY(float dir)
+    {
+        m_speedY += dir * m_deltaSpeedY * Time.deltaTime;
+        m_speedY = Mathf.Clamp(m_speedY, -m_maxSpeedY, m_maxSpeedY);
     }
 
     void FixedUpdate()
     {
+        CheckCollider();
+
         if (m_isJumping)
         {
-            m_rgd.AddForce(new Vector2(GetJumpForceX(), m_jumpForceY));
+            state = State.Jump;
+            if (m_speedY < 0f)
+            {
+                m_speedY = 0f;
+            }
+            UpdateSpeedY(1f);
+            if (m_speedX == 0f)
+            {
+                m_speedX = m_maxSpeedX;
+            }
+        }
+        else
+        {
+            state = State.Fall;
+            UpdateSpeedY(-1f);
         }
 
-        var colliderDir = GetColliderDir();
-        if (colliderDir != ColliderDir.None) {
-            Debug.Log("Current collider is: " + colliderDir);
-            Debug.Log("Current jump dir is: " + m_jumpDir);
-            if (colliderDir == ColliderDir.Right && m_jumpDir == JumpDir.Right)
-            {
-                m_jumpDir = JumpDir.Left;
-            }
-            else if (colliderDir == ColliderDir.Left && m_jumpDir == JumpDir.Left)
-            {
-                m_jumpDir = JumpDir.Right;
-            }
-        }
+        UpdatePosition();
     }
     //private void OnCollisionEnter2D(Collision2D collision)
     //{
@@ -138,7 +173,7 @@ public class ChickController : MonoBehaviour {
     //    return ColliderDir.None;
     //}
 
-    private ColliderDir GetColliderDir()
+    private void CheckCollider()
     {
         SetBoundary();
 
@@ -149,14 +184,16 @@ public class ChickController : MonoBehaviour {
         RaycastHit2D hitBottom2 = Physics2D.Raycast(m_bottom2, Vector2.down, distanceY, 1 << LayerMask.NameToLayer(m_checkColliderLayerName));
         if (hitBottom1.collider != null || hitBottom2.collider != null)
         {
-            return ColliderDir.Down;
+            m_speedY = m_maxSpeedY;
+            Debug.Log("Collider bottom");
         }
 
         RaycastHit2D hitUp1 = Physics2D.Raycast(m_top1, Vector2.up, distanceY, 1 << LayerMask.NameToLayer(m_checkColliderLayerName));
         RaycastHit2D hitUp2 = Physics2D.Raycast(m_top2, Vector2.up, distanceY, 1 << LayerMask.NameToLayer(m_checkColliderLayerName));
         if (hitUp1.collider != null || hitUp2.collider != null)
         {
-            return ColliderDir.Up;
+            m_speedY = -m_maxSpeedY;
+            Debug.Log("Collider up");
         }
 
         RaycastHit2D hitLeft1 = Physics2D.Raycast(m_left1, Vector2.left, distanceX, 1 << LayerMask.NameToLayer(m_checkColliderLayerName));
@@ -164,7 +201,12 @@ public class ChickController : MonoBehaviour {
         RaycastHit2D hitLeft3 = Physics2D.Raycast(m_left3, Vector2.left, distanceX, 1 << LayerMask.NameToLayer(m_checkColliderLayerName));
         if (hitLeft1.collider != null || hitLeft2.collider != null || hitLeft3.collider != null)
         {
-            return ColliderDir.Left;
+            if (m_jumpDir == JumpDir.Left)
+            {
+                m_jumpDir = JumpDir.Right;
+                m_speedX = m_maxSpeedX;
+            }
+            Debug.Log("Collider left");
         }
 
         RaycastHit2D hitRight1 = Physics2D.Raycast(m_right1, Vector2.right, distanceX, 1 << LayerMask.NameToLayer(m_checkColliderLayerName));
@@ -172,24 +214,12 @@ public class ChickController : MonoBehaviour {
         RaycastHit2D hitRight3 = Physics2D.Raycast(m_right2, Vector2.right, distanceX, 1 << LayerMask.NameToLayer(m_checkColliderLayerName));
         if (hitRight1.collider != null || hitRight2.collider != null || hitRight3.collider != null)
         {
-            return ColliderDir.Right;
-        }
-
-        return ColliderDir.None;
-    }
-
-    private float GetJumpForceX()
-    {
-        if (m_jumpDir == JumpDir.Left)
-        {
-            return -m_jumpForceX;
-        }
-        else if (m_jumpDir == JumpDir.Right)
-        {
-            return m_jumpForceX;
-        }
-        else {
-            return 0;
+            if (m_jumpDir == JumpDir.Right)
+            {
+                m_jumpDir = JumpDir.Left;
+                m_speedX = -m_maxSpeedX;
+            }
+            Debug.Log("Collider right");
         }
     }
 
